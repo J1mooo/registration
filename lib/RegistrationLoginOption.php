74be34ff2ed3f89e9a15356746a3777d3e1258ef
J1mooo <77412648+J1mooo@users.noreply.github.com>
2025-11-29 11:50:42 +0100

    Refactor RegistrationLoginOption to use IConfig
    Added configuration check for login button visibility.
    
    Signed-off-by: J1mooo <77412648+J1mooo@users.noreply.github.com>

--- lib/RegistrationLoginOption.php
+++ lib/RegistrationLoginOption.php
@@ -11,26 +11,30 @@ namespace OCA\Registration;
 use OCP\Authentication\IAlternativeLogin;
 use OCP\IL10N;
 use OCP\IURLGenerator;
+use OCP\IConfig;
 
 class RegistrationLoginOption implements IAlternativeLogin {
 
-	public function __construct(
-		protected IURLGenerator $url,
-		protected IL10N $l,
-		protected \OC_Defaults $theming,
-	) {
+	public function __construct(protected IConfig $config, protected IURLGenerator $url, protected IL10N $l, protected \OC_Defaults $theming) {
+		$this->config = $config;
 	}
 
 	public function getLabel(): string {
-		return $this->l->t('Register');
+		if ($this->config->getAppValue('registration', 'login_button_visible', 'yes') === 'yes') {
+			return $this->l->t('Register');
+		}
 	}
 
 	public function getLink(): string {
-		return $this->url->linkToRoute('registration.register.showEmailForm');
+		if ($this->config->getAppValue('registration', 'login_button_visible', 'yes') === 'yes') {
+			return $this->url->linkToRoute('registration.register.showEmailForm');
+		}
 	}
 
 	public function getClass(): string {
-		return 'register-button';
+		if ($this->config->getAppValue('registration', 'login_button_visible', 'yes') === 'yes') {
+			return 'register-button';
+		}
 	}
 
 	public function load(): void {
